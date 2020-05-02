1adbcbe25c9810ebc892f59cd46f502796e191bd
Mitesh Vaghela <miteshvaghela1999@gmail.com>
2020-05-02 21:58:12 +0530

    add clothes and tops

--- lib/src/avataaar.dart
+++ lib/src/avataaar.dart
@@ -1,12 +1,14 @@
 import 'dart:convert' show json;
 
 import 'package:avataaar_image/src/converter.dart';
+import 'package:avataaar_image/src/parts/clothes.dart';
 
 import 'parts/parts.dart';
 
 class Avataaar implements AvataaarPart {
   Avataaar({
-//    this.top,
+    this.top,
+    this.clothes,
     this.eyes,
     this.eyebrow,
     this.mouth,
@@ -15,16 +17,16 @@ class Avataaar implements AvataaarPart {
   });
 
   Avataaar.random({
-//    Top top,
-//    Clothes clothes,
+    Top top,
+    Clothes clothes,
     Eyes eyes,
     Eyebrow eyebrow,
     Mouth mouth,
     Skin skin,
     Style style,
   }) : this(
-//          top: top ?? Top.random,
-//          clothes: clothes ?? Clothes.random,
+          top: top ?? Top.random,
+          clothes: clothes ?? Clothes.random,
           eyes: eyes ?? Eyes.random,
           eyebrow: eyebrow ?? Eyebrow.random,
           mouth: mouth ?? Mouth.random,
@@ -32,8 +34,8 @@ class Avataaar implements AvataaarPart {
           style: style ?? Style.random,
         );
 
-//  final Top top;
-//  final Clothes clothes;
+  final Top top;
+  final Clothes clothes;
   final Eyes eyes;
   final Eyebrow eyebrow;
   final Mouth mouth;
@@ -47,7 +49,7 @@ class Avataaar implements AvataaarPart {
 
   @override
   List<AvataaarPart> get pieces =>
-      [ eyes, eyebrow, mouth, skin, style];
+      [top, clothes, eyes, eyebrow, mouth, skin, style];
 
   MapEntry<String, String> _splitEnum<T>(T enumValue) {
     final split = enumValue.toString().split('\.');
@@ -60,8 +62,8 @@ class Avataaar implements AvataaarPart {
       AvataaarConverter().fromMap(json.decode(value));
 
   Avataaar copyWith({
-//    Top top,
-//    Clothes clothes,
+    Top top,
+    Clothes clothes,
     Eyes eyes,
     Eyebrow eyebrow,
     Mouth mouth,
@@ -69,8 +71,8 @@ class Avataaar implements AvataaarPart {
     Style style,
   }) =>
       Avataaar(
-//        top: top ?? this.top,
-//        clothes: clothes ?? this.clothes,
+        top: top ?? this.top,
+        clothes: clothes ?? this.clothes,
         eyes: eyes ?? this.eyes,
         eyebrow: eyebrow ?? this.eyebrow,
         mouth: mouth ?? this.mouth,
@@ -83,8 +85,8 @@ class Avataaar implements AvataaarPart {
       identical(this, other) ||
       other is Avataaar &&
           runtimeType == other.runtimeType &&
-//          top == other.top &&
-//          clothes == other.clothes &&
+          top == other.top &&
+          clothes == other.clothes &&
           eyes == other.eyes &&
           eyebrow == other.eyebrow &&
           mouth == other.mouth &&
@@ -93,8 +95,8 @@ class Avataaar implements AvataaarPart {
 
   @override
   int get hashCode =>
-//      top.hashCode ^
-//      clothes.hashCode ^
+      top.hashCode ^
+      clothes.hashCode ^
       eyes.hashCode ^
       eyebrow.hashCode ^
       mouth.hashCode ^
@@ -105,8 +107,8 @@ class Avataaar implements AvataaarPart {
 class AvataaarConverter extends Converter<Avataaar> {
   @override
   Avataaar fromMap(Map<String, dynamic> map) => Avataaar(
-//        top: TopConverter().fromMap(map['top']),
-//        clothes: ClothesConverter().fromMap(map['clothes']),
+        top: TopConverter().fromMap(map['top']),
+        clothes: ClothesConverter().fromMap(map['clothes']),
         eyebrow: EyebrowConverter().fromMap(map['eyebrow']),
         eyes: EyesConverter().fromMap(map['eyes']),
         mouth: MouthConverter().fromMap(map['mouth']),
@@ -116,8 +118,8 @@ class AvataaarConverter extends Converter<Avataaar> {
 
   @override
   Map<String, dynamic> toMap(Avataaar value) => {
-//        'top': TopConverter().toMap(value.top),
-//        'clothes': ClothesConverter().toMap(value.clothes),
+        'top': TopConverter().toMap(value.top),
+        'clothes': ClothesConverter().toMap(value.clothes),
         'eyebrow': EyebrowConverter().toMap(value.eyebrow),
         'eyes': EyesConverter().toMap(value.eyes),
         'mouth': MouthConverter().toMap(value.mouth),

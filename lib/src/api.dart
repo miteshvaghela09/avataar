57dba17921b06ec69a4fa4ad24202299602779b3
Mitesh Vaghela <miteshvaghela1999@gmail.com>
2020-05-03 15:04:06 +0530

    add image size13

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -4,14 +4,14 @@ import 'package:avataaar_image/src/avataaar.dart';
 import 'package:http/http.dart' as http;
 
 class AvataaarsApi {
-  static const _BASE_URL = 'http://34.93.92.140:3000/';
+  static const _BASE_URL = 'http://34.93.92.140:3000';
 
   String getUrl(Avataaar avatar, double width) {
     final params = avatar.pieceEntries.map((it) {
       final key = it.key[0].toLowerCase() + it.key.substring(1);
       return '$key=${it.value}';
     }).join('&');
-    return '$_BASE_URL?$params';
+    return '$_BASE_URL/png/?$params';
   }
 
   Future<Uint8List> getImage(Avataaar avatar, double width) =>
